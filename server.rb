d15ef52029e709da69abceffd019bb2ac5b20e89
Yann VERY <yann.very@gmail.com>
2017-02-15 20:30:44 +0100

    Select a collection

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require "json"
+require 'pry'
 
 class Server
 
@@ -6,6 +7,9 @@ class Server
 
   def call(env)
     json_contents = File.read(JSON_PATH)
-    [200, {"Content-Type" => "text/json"}, [json_contents]]
+    data = JSON.load json_contents
+    collection = env['REQUEST_PATH'].gsub('/', '')
+    content = data[collection].to_json
+    [200, {"Content-Type" => "text/json"}, [content]]
   end
 end
