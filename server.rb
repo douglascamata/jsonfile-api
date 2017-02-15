3f3a007fe1bdbed9c1bb8cf3b0614ec6183d2c30
Yann VERY <yann.very@gmail.com>
2017-02-15 22:11:18 +0100

    Try to get one resource

--- server.rb
+++ server.rb
@@ -7,23 +7,30 @@ class Server
 
   def call(env)
     @json_contents = json_contents = File.read(JSON_PATH)
-    collection, id = env['REQUEST_PATH'].split('/')
-    if collection == ''
-      render_all_data
+    collection, id = env['REQUEST_PATH'].split('/')[1..2]
+    if collection && id
+      render_resource(collection, id)
     elsif collection && !id
       render_collections(collection)
     else
-      render_resource(collection, id)
+      render_all_data
     end
   end
 
   def render_all_data
-    [200, {"Content-Type" => "text/json"}, [json_contents]]
+    [200, {"Content-Type" => "text/json"}, [@json_contents]]
   end
 
   def render_collections(collection)
-    data = JSON.load json_contents
+    data = JSON.load @json_contents
     content = data[collection].to_json
+    [200, {"Content-Type" => "text/json"}, [content]]
+  end
+
+  def render_resource(collection, id)
+    data = JSON.load @json_contents
+    binding.pry
+    content = data[collection][id.to_i].to_json
     [200, {"Content-Type" => "text/json"}, [content]]
   end
 end
