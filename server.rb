a311a286472b41fe68abfec4735ac895c2e52d46
Douglas Camata <d.camata@gmail.com>
2017-02-15 20:41:06 +0100

    Add base to render a resource from the json file

--- server.rb
+++ server.rb
@@ -6,9 +6,23 @@ class Server
   JSON_PATH = File.absolute_path("data.json")
 
   def call(env)
-    json_contents = File.read(JSON_PATH)
+    @json_contents = json_contents = File.read(JSON_PATH)
+    collection, id = env['REQUEST_PATH'].split('/')
+    if collection == ''
+      render_all_data
+    elsif collection && !id
+      render_collections(collection)
+    else
+      render_resource(collection, id)
+    end
+  end
+
+  def render_all_data
+    [200, {"Content-Type" => "text/json"}, [json_contents]]
+  end
+
+  def render_collections(collection)
     data = JSON.load json_contents
-    collection = env['REQUEST_PATH'].gsub('/', '')
     content = data[collection].to_json
     [200, {"Content-Type" => "text/json"}, [content]]
   end
