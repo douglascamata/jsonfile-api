564b5c838534270a8a6095ec2ebbc554a4a0c33b
Douglas Camata <d.camata@gmail.com>
2017-02-15 20:20:13 +0100

    Return the whole json on the root of the api

--- server.rb
+++ server.rb
@@ -1,5 +1,11 @@
+require "json"
+
 class Server
+
+  JSON_PATH = File.absolute_path("data.json")
+
   def call(env)
-    [200, {"Content-Type" => "text/plain"}, ["Hello world!"]]
+    json_contents = File.read(JSON_PATH)
+    [200, {"Content-Type" => "text/json"}, [json_contents]]
   end
 end
